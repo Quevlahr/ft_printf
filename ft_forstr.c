c046fbd76b59c45b10a8ca1608fb3ab4253b7cef
Quentin Roulon <quroulon@student.42.fr>
2016-04-29 18:35:07 +0200

    ft_forstr.c

---
+++ ft_forstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_forstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: quroulon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/26 11:34:00 by quroulon          #+#    #+#             */
+/*   Updated: 2016/04/26 11:34:08 by quroulon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void			ft_null_str(t_env *env)
+{
+	ft_putstr("(null)");
+	env->nb_char += 6;
+}
+
+char			*ft_modifstr(char **str, t_env *env)
+{
+	int			i;
+	char		*tmp;
+
+	i = 0;
+	tmp = ft_strnew(ft_strlen(*str));
+	while (str[0][i] && i < env->flag_pt)
+	{
+		tmp[i] = str[0][i];
+		i++;
+	}
+	return (tmp);
+}
